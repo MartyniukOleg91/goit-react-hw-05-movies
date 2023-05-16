6ccd46faf35f7d1437486d8cf189466c1ca96679
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-05-16 14:20:56 +0300

    1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-const API_KEY = 'f80c5afd8737d8b54483408440a37aea';
+const API_KEY = '309df4a19ba58fdd322dab99a0059b3e';
 
 export const fetchTrending = async () => {
   const params = {
