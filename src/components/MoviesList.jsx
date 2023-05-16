4a183f387c6471f57b9b69dffad3fc18c3f16d46
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-05-16 16:05:36 +0300

    fix

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -5,6 +5,7 @@ const MoviesList = ({ movies }) => {
   const location = useLocation();
   const listStyle = {
     listStyle: 'none',
+    height: 30,
   };
 
   return (
@@ -13,6 +14,45 @@ const MoviesList = ({ movies }) => {
         movies.map(movie => {
           return (
             <li key={movie.id} style={listStyle}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+              >
+                <circle cx="12" cy="12" r="10" fill="yellow" />
+                <circle cx="8" cy="9" r="2" fill="black">
+                  <animate
+                    attributeName="cy"
+                    dur="1s"
+                    repeatCount="indefinite"
+                    values="9;15;9"
+                  />
+                </circle>
+                <circle cx="16" cy="9" r="2" fill="black">
+                  <animate
+                    attributeName="cy"
+                    dur="1s"
+                    repeatCount="indefinite"
+                    values="9;15;9"
+                    begin="0.1s"
+                  />
+                </circle>
+                <path
+                  d="M8 15h8M9 17h6"
+                  stroke="black"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                >
+                  <animate
+                    attributeName="stroke"
+                    dur="1s"
+                    repeatCount="indefinite"
+                    values="black;red;black"
+                  />
+                </path>
+              </svg>
               <Link to={`/movies/${movie.id}`} state={{ from: location }}>
                 {movie.title}
               </Link>
